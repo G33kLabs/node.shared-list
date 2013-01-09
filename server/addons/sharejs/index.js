17d567ff57c45c94c2af5578fdc73490334f2b16
G33k <delarueguillaume@gmail.com>
2013-01-09 23:22:17 +0100

    Change redis connection...

--- server/addons/sharejs/index.js
+++ server/addons/sharejs/index.js
@@ -1,11 +1,21 @@
 ///////////////////////////////////////////////////////////// IMPLEMENT SHAREJS AS AN EXPRESS MODULE /////////////
 var sharejs = require('share');
+var parse = require("url").parse;
 
 module.exports = function(server, app) {
 	tools.debug("Load WebServer addon : sharejs") ;
 
+	var redisToGo = process.env.REDISTOGO_URL ? parse(process.env.REDISTOGO_URL) : parse("redis://127.0.0.1:6379"); 
+
+	tools.log('Open connexion to redis : '+JSON.stringify(redisToGo));
+
 	sharejs.server.attach(server, {
-		type: 'connect-heroku-redis',
+		db: {
+			type: 'redis',
+			hostname: redisToGo.hostname,
+			port: redisToGo.port,
+			auth: redisToGo.auth,
+		},
 		browserChannel: {cors: '*'}
 	});
 
