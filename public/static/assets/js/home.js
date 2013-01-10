857c27b1b11653875587632a7b0f82c3046ea5ed
G33k <delarueguillaume@gmail.com>
2013-01-10 16:52:39 +0100

    Fix document toggling

--- public/static/assets/js/home.js
+++ public/static/assets/js/home.js
@@ -1,5 +1,12 @@
 (function () {
 
+    // -> DOM
+    var elems = {
+        editor : $('#pad_editor'),
+        editor_loading: $('#tpl_textarea_default').html(),
+        status: $('#status')
+    }
+
     // -> Overload app
     var connection = null; 
     var activeDoc = null;
@@ -9,22 +16,21 @@
         activeDoc = title; 
 
         // -> Connection status display
-        var status = $('#status').show(0);
+        var status = elems.status.show(0);
 
         // -> Open a shred list
-        var tpl_txt = $('#tpl_textarea_default').html() ;
-        var elem = $('<textarea id="pad" disabled>'+tpl_txt+'</textarea>'); 
+        var textarea = $('<textarea id="pad" disabled>'+elems.editor_loading+'</textarea>'); 
+        elems.editor.css({display: 'block'}) ;
         $('#editor h4').html('Share link : <a href="'+window.location.href+'">'+window.location.href+'</a>')
-        $('#editor').show(0)
-        $('#pad_container').html(elem) ;
+        $('#pad_container').html(textarea) ;
 
         // -> Open connexion
         connection = sharejs.open(activeDoc, 'text', function(error, doc) {
             if (error) {
                 console.log(error);
             } else {
-                elem.get(0).disabled = false;
-                doc.attach_textarea(elem.get(0));
+                textarea.get(0).disabled = false;
+                doc.attach_textarea(textarea.get(0));
             }
             updateShareList() ;
         });  
@@ -32,8 +38,8 @@
         // -> Bind events to status
         var register = function(state, klass, text) {
             connection.on(state, function() {
-                status.attr('class', 'label label-' + klass);
-                status.html(text);
+                elems.status.attr('class', 'label label-' + klass);
+                elems.status.html(text);
                 console.log(state, connection)
             });
         };
@@ -88,7 +94,7 @@
         // -> Update html dropdown list
         var html = '<li><a href="#new">Create new</a></li><li class="divider"></li>'; 
         _.each(docs, function(doc) { 
-            html += '<li><a href="#'+doc.name+'" id="'+doc.name+'">'+doc.name+' <em>('+moment(doc.modified).format('YYYY-MM-DD HH:mm:ss')+')</em></a></li>';
+            html += '<li><a href="#'+doc.name+'" id="'+doc.name+'"><b>'+doc.name+'</b> <em>('+moment(doc.modified).format('YYYY-MM-DD HH:mm:ss')+')</em></a></li>';
         });
         $('#documents_list').html(html);
 
@@ -158,33 +164,42 @@
 	
     });
 
-	// -> First display
-	if(!window.location.hash) $('#pad').hide(0);
+    // -> Manage document toggle
+    var manageDocDisplay = function() {
+        var hash = window.location.hash.slice(1); //hash to string (= "myanchor")
+        console.log("manageDocDisplay::", hash) ;
+        if ( hash && hash != 'home' ) {
+            if ( hash == 'new' ) {
+                hash = generateId(24) ;
+                window.location.hash = hash;
+                return;
+            }
+            openShareList(hash) ;   
+            //console.log($('#editor').offset().top) ;`
+            elems.editor.css({display: 'block'}) ;
+            $('body').stop().animate({'scrollTop': elems.editor.offset().top}, 800) ;
+        }
+        else {
+            $('body').stop().animate({scrollTop: 0}, 800, function() {
+                elems.editor.css({display: 'none'}) ; 
+            });
+        }
+    }
 
 	// -> Bind hash changes
 	$(window).on('hashchange', function () { //detect hash change
-	    var hash = window.location.hash.slice(1); //hash to string (= "myanchor")
-	    if ( hash && hash != 'home' ) {
-	    	if ( hash == 'new' ) {
-	    		hash = generateId(24) ;
-	    		$('#editor h4').html('Create a new list')
-	    		window.location.hash = hash;
-	    		return;
-	    	}
-	    	openShareList(hash) ;	
-	    	$('html,body').animate({'scrollTop': $('#editor').offset().top}, 800)
-	    }
-	    else {
-	    	$('body, html').animate({scrollTop: 0}, 800, function() {
-	    		$('#editor').hide(0)	
-	    	});
-	    }
+	    manageDocDisplay() 
 	});
 
 	// -> Disable effects on hash links
 	$('a[href*=#]').live('click', function(e) {
 		var hash = this.getAttribute('href') ;
-		window.location.hash = hash;
+
+        // If document is already opened
+        //console.log("->" + window.location.hash + ' => ' + hash)
+        window.location.hash = hash;
+
+        // Prevent click
 		e.preventDefault() ;
 	})
 
