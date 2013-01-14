b2ce5f313be4fecc35db19f78b85a619a25ebce3
G33k <delarueguillaume@gmail.com>
2013-01-14 15:57:22 +0100

    Add robots.txt

--- server/addons/sharejs/index.js
+++ server/addons/sharejs/index.js
@@ -1,6 +1,5 @@
 ///////////////////////////////////////////////////////////// IMPLEMENT SHAREJS AS AN EXPRESS MODULE /////////////
 var sharejs = require('share');
-var parse = require("url").parse;
 
 module.exports = function(server, app) {
 	tools.debug("Load WebServer addon : sharejs") ;
